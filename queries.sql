a41a5ce7979bf3fcb3e305c8c12a0a03327adadb
Оксана Сосновских <conmir20@gmail.com>
2026-07-25 14:18:22 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
+--Этот запрос выводит общее количество покупателей
+select count(customer_id) as customers_count from customers;
+
 -- Этот запрос выводит 10 лучших по выручке продавцов
 select concat(c.first_name, ' ', c.last_name) as seller,
 	count(s.sales_id) as operations,
@@ -43,3 +46,54 @@ from (
 		on p.product_id = s.product_id
 	group by day_of_week, sale_date, e.first_name, e.last_name
 	order by day_of_week, seller);
+-- Этот запрос считает количество покупателей в разных возрастных группах
+select age_category,
+	sum(age_count) as age_count
+from (select (
+	case 
+		when age > 40 then '40+'
+		when age < 41 and age > 25 then '26-40'
+		when age < 26 then '16-25'
+		when age < 16 then 'child'
+	end) as age_category,
+	count(age) as age_count,
+	age
+from customers
+group by age)
+group by age_category
+
+--Этот запрос считает количество уникальных покупателей в месяце и выручку, которую они принесли
+select to_char(selling_month, 'YYYY-MM') as selling_month,
+	sum(total_customers) as total_customers,
+	sum(income) as income
+from (select floor(sum(s.quantity*price)) as income, 
+		DATE_TRUNC ('month', s.sale_date) as selling_month,
+		count(s.customer_id) as total_customers,
+		s.sale_date
+	from sales s
+	left join customers c
+		on c.customer_id = s.customer_id
+	left join products p
+		on p.product_id = s.product_id
+		group by extract(month from s.sale_date), s.sale_date)
+group by selling_month
+order by selling_month
+
+-- Этот запрос выводит список покупателей, пришедших по акционному предложению
+select concat(c.first_name, ' ', c.last_name) as customer,
+	sale_date,
+	concat(e.first_name, ' ', e.last_name) as seller
+from (select customer_id,
+	sales_person_id,
+	FIRST_VALUE(s.sale_date) over (order by customer_id) as sale_date
+from sales s
+left join products p 
+	on p.product_id = s.product_id 
+where price = 0) as sub
+left join customers c
+	on c.customer_id = sub.customer_id
+left join employees e
+	on e.employee_id = sub.sales_person_id
+group by concat(c.first_name, ' ', c.last_name), sub.sale_date, concat(e.first_name, ' ', e.last_name),sub.customer_id
+order by sub.customer_id
+
